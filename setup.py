1d9a654cb9ccbb9bffb5736ae5e6c635573e679e
parvez-pathan <ppathan@digitalpharmacist.com>
2018-11-28 11:52:59 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='test_packing',
+      version='0.1',
+      description='The World Of Quotes',
+      url='https://github.com/Parvez-Khan-1/test_packing',
+      author='Parvez Khan',
+      author_email='parvezpathan09@gmail.com',
+      license='MIT',
+      packages=['test_packing'],
+      zip_safe=False)
---
+++ test_packing/quotes.py
@@ -0,0 +1,14 @@
+class Quotes:
+    def __init__(self):
+        self.farewell = ['Sample Farewell Quote 1 ', 'Sample Farewell Quote 2']
+        self.motivational = ['Sample Motivational Quote 1 ', 'Sample Motivational Quote 2']
+        self.educational = ['Sample Educational Quote 1 ', 'Sample Educational Quote 2']
+
+    def farewell_quotes(self):
+        return self.farewell
+
+    def motivational_quotes(self):
+        return self.motivational
+
+    def educational_quotes(self):
+        return self.educational
